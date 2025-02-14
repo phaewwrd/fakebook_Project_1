8fe4b5aa0835dd80f3f0002a37cdd4ac28b8c8ad
phaewphanwrd <phaewphanwrd@gmail.com>
2025-02-14 13:43:33 +0700

    Hi

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1 className='text-3xl border bg-pink-300'>Codecamp Thailand</h1>
+      <button className="btn btn-success">OK</button>
+      </div>
+  )
+}
+
+export default App
